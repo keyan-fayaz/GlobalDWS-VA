0b6058d78ab59fee8ca3449688300451d45337f5
Jamal Zaghmout <jamal.zaghmout@globaldws.com>
2021-08-04 22:56:38 +0300

    Edited Mycroft + MQTT

--- Mycroft-MQTT.py
+++ Mycroft-MQTT.py
@@ -29,15 +29,15 @@ client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
 # set username and password
 client.username_pw_set("JetsonNano", "G1obalDWS")
 
-# connect to HiveMQ Cloud on port 8883
-client.connect("df50a90e1ca64a16a186f38cb2d14114.s2.eu.hivemq.cloud", 8883)
-
-# subscribe to the topic "CovidApp/Activate"
-client.subscribe("CovidApp/Activate")
-
 
 def activate_app():
     print("##### WAKE WORD DETECTED #####\n")
+
+    # connect to HiveMQ Cloud on port 8883
+    client.connect("df50a90e1ca64a16a186f38cb2d14114.s2.eu.hivemq.cloud", 8883)
+
+    # subscribe to the topic "CovidApp/Activate"
+    client.subscribe("CovidApp/Activate")
 
     client.loop_start()
 
@@ -56,5 +56,4 @@ def main():
     runner.start()
 
 
-for i in range(0, 10):
-    main()
+main()
