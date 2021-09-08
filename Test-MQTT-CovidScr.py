72f10613124857bc27d91f6df68b3d2b6d36cc64
Jamal Zaghmout <jamal.zaghmout@globaldws.com>
2021-09-08 21:05:32 +0300

    Implemented local MQTT broker + retrained wake word model

--- Test-MQTT-CovidScr.py
+++ Test-MQTT-CovidScr.py
@@ -1,13 +1,7 @@
-from __future__ import division
-import os
 import time
 from time import sleep
 
-from precise_runner import PreciseEngine, PreciseRunner
 import paho.mqtt.client as mqtt
-
-precise_path = os.path.join(os.path.dirname(__file__), "precise-engine/precise-engine")
-ww_path = os.path.join(os.path.dirname(__file__), "hey-dsr/hey-dsr.pb")
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -29,38 +23,29 @@ mqtt.Client.connected_flag = False  # create flag IN class
 client = mqtt.Client("GDWSPython")
 broker = "192.168.0.151"
 
+client.on_connect = on_connect
+
 print("Connecting to broker", broker)
 client.connect(broker)
+
+client.loop_start()
 
 while not client.connected_flag:  # wait in loop
     print("In wait loop")
     time.sleep(1)
 
-client.on_connect = on_connect
-client.on_message = on_message
+client.publish("CovidApp/Activate", "ACTIVATE")
+print("MQTT message sent")
 
+client.loop_stop()
+client.disconnect()  # disconnect
+print("Disconnected successfully")
 
-def activate_app():
-    print("##### WAKE WORD DETECTED #####")
-
-    client.loop_start()
-
-    client.subscribe("CovidApp/Activate")
-    client.publish("CovidApp/Activate", "ACTIVATE")
-    print("MQTT message sent\n")
-
-    client.loop_stop()
-
-
-def main():
-    engine = PreciseEngine(precise_path, ww_path)
-    runner = PreciseRunner(engine, on_activation=lambda: activate_app())
-    runner.start()
-
-    # Sleep forever
-    while True:
-        sleep(100)
-
-
-for i in range(0, 1):
-    main()
+# # subscribe to the topic "CovidApp/Activate"
+# client.subscribe("CovidApp/Activate")
+#
+# client.on_message = on_message
+#
+# client.publish("CovidApp/Activate", "ACTIVATE")
+#
+# client.loop_forever()
